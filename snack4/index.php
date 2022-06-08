0a76c51403b78c14038abe2e3ff04db840603d0d
Paparellav <vitantoniopaparella@gmail.com>
2022-06-08 16:08:45 +0200

    snack 4 completed

---
+++ snack4/index.php
@@ -0,0 +1,27 @@
+<!-- ## Snack 4 (Bonus)
+Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  $integersArray = [];
+  while (count($integersArray) < 15) {
+    $newInt = rand(1, 100);
+    if (!in_array($newInt, $integersArray)) {
+      array_push($integersArray, $newInt);
+    }
+  }
+  var_dump($integersArray);
+  ?>
+</body>
+
+</html>
